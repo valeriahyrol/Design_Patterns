f25d053f4e24ce4006304e4a5c045363b2ec6d11
valeriahyrol <117359144+valeriahyrol@users.noreply.github.com>
2024-05-06 11:37:57 +0300

    Update FileWriter.cs

--- lab_3_hyrol/lab_3_hyrol/Adapter/FileWriter.cs
+++ lab_3_hyrol/lab_3_hyrol/Adapter/FileWriter.cs
@@ -10,13 +10,11 @@ namespace lab_3_hyrol
     {
         public void Write(string message)
         {
-            // Реалізація запису у файл
             Console.WriteLine($"Writing to file: {message}");
         }
 
         public void WriteLine(string message)
         {
-            // Реалізація запису у файл з переходом на новий рядок
             Console.WriteLine($"Writing to file: {message}");
         }
     }
